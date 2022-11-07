cb57c8e6a6dbbf85ec128622a353ad6679d9c2c2
Oscar Rivera <orivera@applaudostudios.com>
2022-11-06 20:43:14 -0600

    feat: add stripe for payment

--- atoms/FormularioDePago.js
+++ atoms/FormularioDePago.js
@@ -1,126 +1,90 @@
 import React from "react";
-import { Text, View, TextInput } from "react-native";
+import { View, Alert, StyleSheet } from "react-native";
 import Button from "./Button";
 import { useNavigation } from "@react-navigation/native";
-import { useForm, Controller } from "react-hook-form";
+import {
+  StripeProvider,
+  useConfirmPayment,
+  CardField,
+} from "@stripe/stripe-react-native";
 
-const FormularioDePago = ({plan}) => {
+const API_URL = "https://shy-opalescent-bactrosaurus.glitch.me";
+const PUBLISHABLE_KEY =
+  "pk_test_51Ho4m5A51v44wNexXNFEg0MSAjZUzllhhJwiFmAmJ4tzbvsvuEgcMCaPEkgK7RpXO1YI5okHP08IUfJ6YS7ulqzk00O2I0D1rT";
+
+const FormularioDePago = ({ plan }) => {
+  const { confirmPayment, loading } = useConfirmPayment();
+
   const navigation = useNavigation();
-  const {
-    handleSubmit,
-    control,
 
-    formState: { errors },
-  } = useForm();
+  const fetchPaymentIntentClientSecret = async () => {
+    const response = await fetch(`${API_URL}/create-payment-intent`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        currency: "usd",
+        amount: 7,
+        items: [{ id: "id" }],
+      }),
+    });
+    const { clientSecret } = await response.json();
 
-  const onSubmit = (data) => {
-    // console.log(data);
-     navigation.navigate("pagoExitoso", {
-       planId: plan,
-     });
+    return clientSecret;
   };
 
+  const onSubmit = async () => {
+    // 1. fetch Intent Client Secret from backend
+    const clientSecret = await fetchPaymentIntentClientSecret();
 
+    const billingDetails = {
+      email: "email@stripe.com",
+      phone: "+48888000888",
+      addressCity: "Houston",
+      addressCountry: "US",
+      addressLine1: "1459  Circle Drive",
+      addressLine2: "Texas",
+      addressPostalCode: "77063",
+    }; // mocked data for development account
+
+    const { error, paymentIntent } = await confirmPayment(clientSecret, {
+      paymentMethodType: "Card",
+      billingDetails,
+    });
+
+    if (error) {
+      Alert.alert(`Lo sentimos no pudimos procesar su pago -${error}`);
+    } else if (paymentIntent) {
+      navigation.navigate("pagoExitoso", {
+        planId: plan,
+      });
+    }
+  };
 
   return (
-    <View>
-      <Text className="text-base text-white mx-0 my-5">Número de tarjeta</Text>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-            placeholder="**** **** ****"
-            className="bg-black	border-b-[#4D7FAE]  border border-y-500	text-white p-[5px] h-7"
-            placeholderTextColor="rgba(255, 255, 255, 0.5)"
-          />
-        )}
-        name="tarjeta"
-        rules={{ required: true }}
-      />
-
-      <View className="flex flex-row justify-between">
-        <View>
-          <Text className="text-base text-white mx-0 my-5">Vencimiento</Text>
-          <View className="flex flex-row">
-            <View>
-              <Controller
-                control={control}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <TextInput
-                    onBlur={onBlur}
-                    onChangeText={(value) => onChange(value)}
-                    value={value}
-                    placeholder="MM"
-                    className="bg-black	border-b-[#4D7FAE]  border border-y-500	text-white p-[5px] h-7 w-[40px]"
-                    placeholderTextColor="rgba(255, 255, 255, 0.5)"
-                  />
-                )}
-                name="mes"
-                rules={{ required: true }}
-              />
-            </View>
-            <View>
-              <Controller
-                control={control}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <TextInput
-                    onBlur={onBlur}
-                    onChangeText={(value) => onChange(value)}
-                    value={value}
-                    placeholder="/YYYY"
-                    className="bg-black	border-b-[#4D7FAE]  border border-y-500	text-white p-[5px] h-7 w-[60px]"
-                    placeholderTextColor="rgba(255, 255, 255, 0.5)"
-                  />
-                )}
-                name="año"
-                rules={{ required: true }}
-              />
-            </View>
-          </View>
-        </View>
-        <View>
-          <Text className="text-base text-white mx-0 my-5">Cvv</Text>
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                onBlur={onBlur}
-                onChangeText={(value) => onChange(value)}
-                value={value}
-                placeholder="CVV"
-                className="bg-black	border-b-[#4D7FAE]  border border-y-500	text-white p-[5px] h-7 w-[124px]"
-                placeholderTextColor="rgba(255, 255, 255, 0.5)"
-              />
-            )}
-            name="cvv"
-            rules={{ required: true }}
-          />
-        </View>
+    <StripeProvider publishableKey={PUBLISHABLE_KEY}>
+      <View style={styles.container}>
+        <CardField
+          postalCodeEnabled={false}
+          autofocus
+          style={styles.cardField}
+          cardStyle={{
+            textColor: "#1c1c1c",
+          }}
+        />
+        {!loading && <Button onSubmit={onSubmit} text="Procesar" />}
       </View>
-      <Text className="text-base text-white mx-0 my-5">Titular</Text>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-            placeholder="Nombre del propietario"
-            className="bg-black	border-b-[#4D7FAE]  border border-y-500	text-white p-[5px] h-7"
-            placeholderTextColor="rgba(255, 255, 255, 0.5)"
-          />
-        )}
-        name="nombre"
-        rules={{ required: true }}
-      />
-      <View>
-        <Button text="Procesar" onSubmit={onSubmit} />
-      </View>
-    </View>
+    </StripeProvider>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cardField: {
+    height: 40,
+  },
+});
 export default FormularioDePago;
